79f9b033bf7fb325f980bb100708478677f88a7c
Yashil Busa <98388548+21it016@users.noreply.github.com>
2023-01-19 12:56:04 +0530

    Add files via upload

---
+++ IT255 WEB TECHNOLOGIES/HTML FILES/lab5.js
@@ -0,0 +1,85 @@
+a=document.getElementById("HELOO");
+
+setInterval(
+function()
+{
+    let randomColor = Math.floor(Math.random()*19142004).toString(16);
+
+    document.body.style.backgroundColor = "#"+randomColor;
+}
+
+,1000);
+const bike={
+    compony:"Royal enfield",
+    price:"1laC",
+    color:"black",
+    type:"bike",
+
+}
+const phone={
+    compony:"Apple",
+    model:"iphone14",
+    price:"1.5LAC",
+    color:"Gray",
+    type:"PHONE",
+}
+
+const car={
+    compony:"Mercedece",
+    price:"2.5CR",
+
+}
+idcame=""
+function MYFUN(idcame){
+    console.log("HELLO")
+    switch(idcame)
+    {
+        case "CAR":
+          for(i in car){
+           tr = document.createElement("tr");
+           const trkey =document.createElement("td");
+           const rvalue = document.createElement("td");
+            trkey.textContent = i;
+            rvalue.textContent= car[i];
+           
+            tr.appendChild(trkey);
+            tr.appendChild(rvalue);
+
+            document.getElementById("id1").appendChild(tr);
+            
+          }  
+          break;
+
+        case "phone":
+            for(i in phone){
+              
+                tr = document.createElement("tr");
+                const trkey =document.createElement("td");
+                const rvalue = document.createElement("td");
+                 trkey.textContent = i;
+                 rvalue.textContent= phone[i];
+                 tr.appendChild(trkey);
+                 tr.appendChild(rvalue);
+                 //document.getElementById("id1").innerHTML=null;
+
+                 document.getElementById("id1").appendChild(tr);
+                 
+               }  
+            console.log(phone);
+            break;
+
+        case "BIKE":
+            for(i in bike){
+                tr = document.createElement("tr");
+                const trkey =document.createElement("td");
+                const rvalue = document.createElement("td");
+                 trkey.textContent = i;
+                 rvalue.textContent= bike[i];
+                 tr.appendChild(trkey);
+                 tr.appendChild(rvalue);
+                
+                 document.getElementById("id1").appendChild(tr);
+               }  
+            break;
+    }
+}
